3e286472ad7ba4ea441421aeec4e711991880d80
AlexToday111 <ernestkudakaev6@mail.ru>
2026-01-15 11:49:44 +0300

    Add dto/mapper

---
+++ src/main/java/com/example/todo/mapper/TaskMapper.java
@@ -0,0 +1,23 @@
+package com.example.todo.mapper;
+
+import com.example.todo.dto.TaskRequestDto;
+import com.example.todo.dto.TaskResponseDto;
+import com.example.todo.model.Task;
+
+public class TaskMapper {
+    public static TaskResponseDto toDto(Task task) {
+        TaskResponseDto dto = new TaskResponseDto();
+        dto.setId(task.getId());
+        dto.setTitle(task.getTitle());
+        dto.setDescription(task.getDescription());
+        dto.setCompleted(task.isComplete());
+        return dto;
+    }
+    public static Task toEntity(TaskRequestDto dto) {
+        Task task = new Task();
+        task.setTitle(dto.getTitle());
+        task.setDescription(dto.getDescription());
+        task.setComplete(dto.isCompleted());
+        return task;
+    }
+}
